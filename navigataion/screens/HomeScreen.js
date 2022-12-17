09c74e7c2b17d6c5f33a09d79764000ec44eea18
dwainejade <dwainejade@gmail.com>
2022-12-17 17:25:14 -0500

    bottom nav working

---
+++ navigataion/screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native'
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>HomeScreen</Text>
+        </View>
+    )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+
+})
