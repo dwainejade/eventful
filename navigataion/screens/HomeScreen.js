7c9a35309d4c25b33ed3653838f016a44ea5b4d0
dwainejade <dwainejade@gmail.com>
2022-12-19 13:02:35 -0500

    styled header and card padding

--- navigataion/screens/HomeScreen.js
+++ navigataion/screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import EventCard from '../../components/EventCard';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const data = require('../../data/MOCK_DATA.json');
 
@@ -8,7 +9,27 @@ const HomeScreen = () => {
     return (
         <SafeAreaView style={styles.container}>
             <ScrollView>
-                <Text>Featured</Text>
+
+                <View style={styles.searchContainer} >
+                    <TextInput style={styles.searchBar} >
+                        <Ionicons name='search' size={15} />
+                    </TextInput>
+                    <TouchableOpacity style={styles.locationButton}>
+                        <Ionicons style={styles.locationIcon} name='location' size={20} />
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.headingContainer}>
+                    <Text style={styles.heading}>Featured</Text>
+                    <TouchableOpacity><Text>View all</Text></TouchableOpacity>
+                </View>
+                {/* featured cards are wide */}
+
+                <View style={styles.headingContainer}>
+                    <Text style={styles.heading}>Trending</Text>
+                    <TouchableOpacity><Text>View all</Text></TouchableOpacity>
+                </View>
+
                 <ScrollView
                     horizontal={true}
                     showsHorizontalScrollIndicator={false}
@@ -21,9 +42,11 @@ const HomeScreen = () => {
                     )
                     )}
                 </ScrollView>
-                <Text>Trending</Text>
 
-                <Text>Nearby</Text>
+                <View style={styles.headingContainer}>
+                    <Text style={styles.heading}>Nearby</Text>
+                    <TouchableOpacity><Text>View all</Text></TouchableOpacity>
+                </View>
 
             </ScrollView>
         </SafeAreaView>
@@ -38,8 +61,44 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         flexDirection: 'row'
     },
+    headingContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        margin: 2,
+        marginHorizontal: 6,
+        height: 16
+    },
+    heading: {
+        fontWeight: 'bold',
+    },
     text: {
         fontSize: 25,
         fontWeight: '500',
+    },
+    searchContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        marginHorizontal: 10,
+        marginVertical: 6
+    },
+    searchBar: {
+        backgroundColor: '#E9EBED',
+        flex: 1,
+        height: 30,
+        padding: 8,
+        paddingHorizontal: 12,
+        borderRadius: 15,
+        marginRight: 8
+    },
+    locationButton: {
+        height: 30,
+        width: 30,
+        backgroundColor: '#E9EBED',
+        borderRadius: 8,
+        padding: 3,
+    },
+    locationIcon: {
+        alignSelf: 'center'
     }
 });
