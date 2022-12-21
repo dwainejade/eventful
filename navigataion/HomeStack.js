a2b1315ad6ffb4969fe308ac319ca86863a81ae5
dwainejade <dwainejade@gmail.com>
2022-12-21 09:13:06 -0500

    hiding bottom nav on details screen

--- navigataion/HomeStack.js
+++ navigataion/HomeStack.js
@@ -2,17 +2,28 @@ import React from 'react'
 import HomeScreen from './screens/HomeScreen';
 import EventDetailsScreen from './screens/EventDetailsScreen';
 import { createStackNavigator } from '@react-navigation/stack';
+import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
 
 const Stack = createStackNavigator();
 
-export default function HomeStack() {
+export default function HomeStack({ navigation, route }) {
+    React.useLayoutEffect(() => {
+        const tabHiddenRoutes = ["EventDetails", "PurchaseScreen"];
+
+        const routeName = getFocusedRouteNameFromRoute(route);
+        if (tabHiddenRoutes.includes(getFocusedRouteNameFromRoute(route))) {
+            navigation.setOptions({ tabBarStyle: { display: 'none' } });
+        } else {
+            navigation.setOptions({ tabBarStyle: { display: 'flex' } });
+        }
+    }, [navigation, route]);
     return (
-        <Stack.Navigator initialRouteName="Home"
+        <Stack.Navigator initialRouteName="HomeScreen"
             screenOptions={{
                 headerShown: false
             }}
         >
-            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="HomeScreen" component={HomeScreen} />
             <Stack.Screen name="EventDetails" component={EventDetailsScreen} />
         </Stack.Navigator>
     );
