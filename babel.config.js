31ff2234066ac9a1f9ceedddddd02d9239b7e4e4
dwainejade <dwainejade@gmail.com>
2022-12-22 14:22:59 -0500

    added supabase and dotenv

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
-module.exports = function(api) {
+require('dotenv').config({ path: './.env.development' })
+console.log('REACT_APP_BASE_URL: ' + process.env.REACT_APP_BASE_URL)
+
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    env: {
+      production: {
+        plugins: [["inline-dotenv", {
+          path: '.env.production'
+        }]]
+      },
+      development: {
+        plugins: [["inline-dotenv", {
+          path: '.env.development'
+        }]]
+      }
+    }
   };
 };
---
+++ supabase/supabase.js
@@ -0,0 +1,3 @@
+const supabaseUrl = 'https://puzyfhvigchddgzbojnf.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
