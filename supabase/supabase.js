6404894e1dc7646e08cacd2e5df8c863be813348
dwainejade <dwainejade@gmail.com>
2022-12-23 11:16:24 -0500

    auth and async  storage added

--- supabase/supabase.js
+++ supabase/supabase.js
@@ -1,6 +1,13 @@
-
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://puzyfhvigchddgzbojnf.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+    auth: {
+        storage: AsyncStorage,
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: false,
+    },
+})
