0eedcc942a52fa5bfe80443e921e2c9b1abd0995
dwainejade <dwainejade@gmail.com>
2023-01-06 16:22:51 -0500

    added QR Code

--- components/QRCodeGenerator.js
+++ components/QRCodeGenerator.js
@@ -1,34 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import * as Crypto from 'expo-crypto';
+import React, { useEffect } from 'react';
 import QRCode from 'react-native-qrcode-svg'
-import { ActivityIndicator } from 'react-native'
+import { View } from 'react-native'
 
 
-export default function QRCodeGenerator({ ticketInfo }) {
-  const [code, setCode] = useState(null)
-
-  useEffect(() => {
-    (async () => {
-      const digest = await Crypto.digestStringAsync(
-        Crypto.CryptoDigestAlgorithm.SHA256,
-        ticketInfo
-      );
-      setCode(digest)
-    })();
-  }, []);
+export default function QRCodeGenerator({ ticketInfo, size }) {
 
   return (
-    <>
-      {code ?
-        <QRCode
-          value={code}
-          size={100}
-          color="#000"
-          backgroundColor="#fff"
-        />
-        :
-        <ActivityIndicator />
-      }
-    </>
+    <View>
+      <QRCode
+        value={ticketInfo}
+        size={size}
+        color="#000"
+        backgroundColor="#fff"
+      />
+    </View>
   );
 }
